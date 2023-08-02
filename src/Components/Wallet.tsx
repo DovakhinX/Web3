765cd6744c927f329ff35c9f329563abf8d5870a
DovakhinX <sunwin60@gmail.com>
2023-08-03 00:15:27 +0530

    connect logic small adjustment

--- src/Components/Wallet.tsx
+++ src/Components/Wallet.tsx
@@ -5,7 +5,7 @@ import { useAccount, useConnect, useBalance } from 'wagmi'
 export default function Wallet() {
 
     const { connect, connectors } = useConnect()
-    const { address, isConnected } = useAccount()
+    const { address, isConnected,isDisconnected,status } = useAccount()
     const { data: balance } = useBalance({ address })
 
     return (
@@ -14,17 +14,20 @@ export default function Wallet() {
             {connectors.map((connector) => (
                 <button className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-xl"
                     key={connector.id}
+                    disabled={isConnected}
                     onClick={() => connect({ connector })}>
-                    {connector.name}
+                    {connector.name} ({status})
                 </button>))}
-            {isConnected ? (
+            {isConnected?(
                 <div className="flex flex-col items-start gap-4  border-2 border-lime-200 w-[650px] h-[200px] rounded-md shadow-xl p-4 bg-lime-200 ">
                     <p className="text-xl ">Account: {address}</p>
-                    <p className="text-xl "> Balance: {balance ? `${balance.value}` : `0`}</p>
+                    <p className="text-xl "> Balance: {balance ? `${balance.formatted} ${balance.symbol}` : `0`}</p>
                 </div>
-            ) : (<div className="flex flex-col items-center justify-center  border-2 border-lime-200 w-[400px] h-[200px] rounded-md shadow-xl p-4  bg-slate-500  ">
-                <p className="text-2xl ">Please connect your wallet</p>
-            </div>)
+            ):isDisconnected ? (
+                <div className="flex flex-col items-center justify-center  border-2 border-lime-200 w-[400px] h-[200px] rounded-md shadow-xl p-4  bg-slate-500  ">
+                <p className="text-2xl "> Please connect your wallet</p>
+            </div>
+            ):(null)
             }
         </div>
     )
