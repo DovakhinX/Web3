0251f6a1081a53240038eac3528da833bee38b79
DovakhinX <sunwin60@gmail.com>
2023-08-04 23:17:14 +0530

    fix?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import dynamic from 'next/dynamic'
 import { WagmiConfig } from "wagmi"
-import { config } from '../components/Wagmi'
+import { config } from '@/components/Wagmi'
 
 
-const Wallet = dynamic(() => import('../components/Wallet'), { ssr: false })
-const Transfer = dynamic(() => import('../components/Transfer'), { ssr: false })
+const Wallet = dynamic(() => import('@/components/Wallet'), { ssr: false })
+const Transfer = dynamic(() => import('@/components/Transfer'), { ssr: false })
 
 
 export default function Home() {
