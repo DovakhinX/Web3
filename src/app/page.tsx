462c3fc614ea0e702441453f8ce1404bf26c77c9
DovakhinX <sunwin60@gmail.com>
2023-08-04 23:53:18 +0530

    xf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
-import dynamic from 'next/dynamic'
+//import dynamic from 'next/dynamic'
 import { WagmiConfig } from "wagmi"
 import { config } from '../components/Wagmi'
+import Wallet from '../components/Wallet'
+import Transfer from '../components/Transfer'
 
 
-const Wallet = dynamic(() => import('../components/Wallet'), { ssr: false })
-const Transfer = dynamic(() => import('../components/Transfer'), { ssr: false })
+//const Wallet = dynamic(() => import('../components/Wallet'), { ssr: false })
+//const Transfer = dynamic(() => import('../components/Transfer'), { ssr: false })
 
 
 export default function Home() {
   return (
     <WagmiConfig config={config}>
       <main className="flex min-h-screen flex-col items-center justify-between p-20 bg-red-400">
-        {Wallet?<Wallet />:null}
-        {Transfer?<Transfer />:null}
+        <Wallet />
+        <Transfer />
       </main>
     </WagmiConfig>
   )
