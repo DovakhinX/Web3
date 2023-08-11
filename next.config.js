fcc9b92e00bef5fb3dd756a95c5b97b60673725b
DovakhinX <sunwin60@gmail.com>
2023-08-11 12:35:53 +0530

    fix3?

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig =  {
+    webpack: (config, { isServer }) => {
+    // Add a rule to handle JSON imports
+    config.module.rules.push({
+      test: /\.json$/,
+      type: 'javascript/auto',
+      use: 'json-loader',
+    });
+
+    return config;
+  },
+
+
+}
 
 module.exports = nextConfig
